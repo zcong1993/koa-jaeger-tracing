b6c3b236c3537d236a21593dc0bc525b9fd47905
zcong1993 <zhangcong1992@gmail.com>
2020-03-17 16:04:56 +0800

    test: wait more time for span

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -87,7 +87,7 @@ it('should work well', async () => {
   await request.get('/test?throw=1')
   await request.get('/notFound')
 
-  await sleep(500)
+  await sleep(1000)
 
   const res1 = await axios.get(
     'http://localhost:16686/api/traces?limit=20&lookback=1m&service=jest-test&operation=koa-tracing-mw'
